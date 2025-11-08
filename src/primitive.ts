8f65b0efea4c3e68639975af068238031f3cff63
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-08 23:15:23 +0600

    primitive data type in ts explore done

---
+++ src/primitive.ts
@@ -0,0 +1,12 @@
+//Ts -- never, unknown, void
+
+let userName: string = 'Shantunu';
+userName.toFixed()
+
+let userId = 123;
+userId.toFixed()
+
+let isAdmin : boolean = false;
+isAdmin = true
+
+let x:undefined = undefined;
