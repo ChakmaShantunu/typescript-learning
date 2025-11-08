b0f84554e2226255289d79d34f2daf80b4a015e5
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-08 23:53:20 +0600

    nonPrimitive type in ts explore done

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,16 @@
+//array, object
+
+//tuple
+
+let bazarList: string[] = ["eggs", "beef", "rice"];
+bazarList.push(12);
+
+let mixedArr: (string | number | boolean)[] = [12, true, "banana"];
+
+let coordinates: [number, number] = [10, 20];
+
+let couple: [string, string] = ["Husband", "Wife"];
+
+let nameAndRoll: [string, number] = ["Shantunu", 17];
+
+let destination: [string, string, number] = ["Dhaka", "Chittagong", 3];
