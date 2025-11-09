1e9ff435c1fc8a7d5fbe5ac06b6ac4f46b400936
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-09 09:50:44 +0600

    object define and optional type define with ts explore done

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -1,16 +1,50 @@
 //array, object
 
-//tuple
+//ts - tuple
 
-let bazarList: string[] = ["eggs", "beef", "rice"];
-bazarList.push(12);
+// let bazarList: string[] = ["eggs", "beef", "rice"];
+// bazarList.push("apple");
 
-let mixedArr: (string | number | boolean)[] = [12, true, "banana"];
+// let mixedArr: (string | number | boolean)[] = [12, true, "banana"];
 
-let coordinates: [number, number] = [10, 20];
+// let coordinates: [number, number] = [10, 20];
 
-let couple: [string, string] = ["Husband", "Wife"];
+// let couple: [string, string] = ["Husband", "Wife"];
 
-let nameAndRoll: [string, number] = ["Shantunu", 17];
+// let nameAndRoll: [string, number] = ["Shantunu", 17];
 
-let destination: [string, string, number] = ["Dhaka", "Chittagong", 3];
+// let destination: [string, string, number] = ["Dhaka", "Chittagong", 3];
+
+//Refference type -- objec
+
+// const user: {
+//     organization: "Programming Hero"; //value => type: literal types
+//     firstName: string;
+//     middleName?: string;  //optional type define
+//     lastName: string;
+//     isMarried: boolean;
+// } = {
+//     organization: "Programming Hero",
+//     firstName: "Shantunu",
+//     middleName: "",
+//     lastName: "Chakma",
+//     isMarried: false,
+// };
+// user.organization = "Programming Hero";
+
+const user: {
+    readonly organization: string; //access modifier
+    firstName: string;
+    middleName?: string;  //optional type define
+    lastName: string;
+    isMarried: boolean;
+} = {
+    organization: "Programming Hero",
+    firstName: "Shantunu",
+    middleName: "",
+    lastName: "Chakma",
+    isMarried: false,
+};
+user.organization = "Programming Hero";
+
+console.log(user.organization);
