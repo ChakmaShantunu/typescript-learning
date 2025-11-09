096edea2ace3d77af1185c15ec24ac3ff113e8ca
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-09 13:21:53 +0600

    nullable, unknown and never type explore in ts done

--- src/nullableUnknownNeverType.ts
+++ src/nullableUnknownNeverType.ts
@@ -2,3 +2,38 @@
 // Unknown
 // NeverType
 
+const getUser = (input: string | null) => {
+    if (input) {
+        console.log(`From DB: ${input}`);
+    } else {
+        console.log('From DB: ALL USER');
+    }
+};
+
+getUser(null);
+
+// Unknown type
+
+const discountCalculator = (input: unknown) => {
+    if (typeof input === 'number') {
+        const discountedPrice = input * 0.1
+        console.log(discountedPrice);
+    } else if (typeof input === 'string') {
+        const [discountedPrice] = input.split(" ");
+        console.log(Number(discountedPrice) * 0.1);
+    } else {
+        console.log('Wrong input');
+    }
+};
+
+discountCalculator(100);
+discountCalculator('100 TK');
+discountCalculator(null);
+
+//void
+
+// const thorwError = (msg: string): never => {
+//     throw new Error(msg);
+// };
+
+// thorwError('Error...');
