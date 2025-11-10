1eedda8ffda359ca7119bd97becb9a9b7e71239c
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-10 22:57:46 +0600

    generics type explore done

---
+++ src/generics.ts
@@ -0,0 +1,28 @@
+// dynamically generalze : Generic
+
+type GenericArray<T> = Array<T>;
+
+// const friends: string[] = ['a', 'b', 'c'];
+const friends: GenericArray<string> = ['a', 'b', 'c'];
+
+// const rollNumber: number[] = [4, 2, 1, 11];
+const rollNumber: GenericArray<number> = [4, 2, 1, 11];
+
+// const isEligibleList: boolean[] = [true, false, true]; 
+const isEligibleList: GenericArray<boolean> = [true, false, true];
+
+
+type Coordinates<X, Y> = [X, Y];
+const coordinates1: Coordinates<number, number> = [20, 30];
+const coordinates2: Coordinates<string, string> = ['20', '30'];
+
+const userList: GenericArray<{ name: string, age: number }> = [
+    {
+        name: 'shantunu',
+        age: 30
+    },
+    {
+        name: 'supta',
+        age: 23
+    }
+]
