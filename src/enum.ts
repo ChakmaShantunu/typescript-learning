0f181272070d912cc27b060b3182273c1750486b
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-11 10:33:55 +0600

    enum explore done

---
+++ src/enum.ts
@@ -0,0 +1,24 @@
+// enum -- 
+
+// set of fixed string leteral ek jaigai rakhe
+
+// type UserRoles = 'admin' | 'editor' | 'viewer';
+
+enum UserRoles {
+    admin = 'admin',
+    editor = 'editor',
+    viewer = 'viewer',
+};
+
+
+
+const canEdit = (role: UserRoles) => {
+
+    if (role === UserRoles.admin || role === UserRoles.editor) {
+        return true
+    }
+    else return false;
+};
+
+const isEditPermissable = canEdit(UserRoles.admin);
+console.log(isEditPermissable);
